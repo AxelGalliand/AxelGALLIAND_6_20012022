f2bbda75480369f8e7051080ba4eb3663115527b
Axel Galliand <galliandaxel88@gmail.com>
2022-02-03 20:52:05 +0100

    Amelioration du usercardDOM

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,14 +1,46 @@
+
+function photographerFactory(data) {
+  const { name, city, country, tagline, price, portrait } = data;
+
+  const picture = `Sample_Photos/Photographers_ID_Photos/${portrait}`;
+
+  function getUserCardDOM() {
+      const article = document.createElement( 'article' );
+      const img = document.createElement( 'img' );
+      img.setAttribute("src", picture)
+      const h2 = document.createElement( 'h2' );
+      h2.textContent = name;
+      const para = document.createElement( 'p' );
+      para.textContent = city;
+      //para.textContent = country;
+      const tag = document.createElement( 'p' );
+      tag.textContent = tagline;
+      const prices = document.createElement( 'p' );
+      prices.textContent = price;
+     // prices.textContent = "€/jours";
+      article.appendChild(img);
+      article.appendChild(h2);
+      article.appendChild(para);
+      article.appendChild(tag);
+      article.appendChild(prices);
+      return (article);
+  }
+  return { name, city, country, tagline, price, picture, getUserCardDOM }
+}
+
+
+
 async function getPhotographers() {
   // Penser à remplacer par les données récupérées dans le json
   const photographers = [
       {
-          "name": "Ma data test",
-          "id": 1,
-          "city": "Paris",
-          "country": "France",
-          "tagline": "Ceci est ma data test",
-          "price": 400,
-          "portrait": "account.png"
+          name: "Ma data test",
+          id: 1,
+          city: "Paris",
+          country: "France",
+          tagline: "Ceci est ma data test",
+          price: 400,
+          portrait: "MimiKeel.jpg"
       },
       {
           "name": "Autre data test",
@@ -17,12 +49,12 @@ async function getPhotographers() {
           "country": "UK",
           "tagline": "Ceci est ma data test 2",
           "price": 500,
-          "portrait": "account.png"
+          "portrait": "RhodeDubois.jpg"
       },
   ]
   // et bien retourner le tableau photographers seulement une fois
   return ({
-      photographers: [...photographers, ...photographers, ...photographers]})
+      photographers: [...photographers]});
 }
 
 async function displayData(photographers) {
