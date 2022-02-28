6b53d69372e1d00327ab93e9173d0171915b3e08
Axel Galliand <galliandaxel88@gmail.com>
2022-02-28 19:39:19 +0100

    modificatoin de la parti porfolio

--- scripts/pages/photographers.js
+++ scripts/pages/photographers.js
@@ -1,3 +1,5 @@
+import {displayModal, closeModal} from "../utils/contactForm.js"
+
 async function fetchPhotographers() {
   return fetch("../../../data/photographers.json")
   .then(function(result) {
@@ -7,48 +9,46 @@ async function fetchPhotographers() {
   })
 }
 
-// const getPhotographerData = async (UserId) => {
-//   const data = await fetchPhotographers ();
-//   const selectedPhotographer = data.photographers.filter ((photographer) => photographer.id === UserId ) [0]
-// console.log(selectedPhotographer);
-//   return getPhotographerData;
-// }
-
-// const getMediaData = async () => {
-//   const data = await fetchPhotographers ();
-//   return data.media;
-// }
+const getPhotographerData = async (UserId) => {
+  const data = await fetchPhotographers ();
+  const selectedPhotographer = data.photographers.filter((photographer) => photographer.id === UserId ) [0]
+  return selectedPhotographer;
+}
 
 const photographer_Id = Number (new URLSearchParams(window.location.search).get("id"));
 
-async function photographerHeader () {
-  const data = await fetchPhotographers ();
-  const selectedPhotographer = data.photographers.filter ((photographer) => photographer.id === photographer_Id ) [0]
-
+async function photographerHeader (photographer) {
   const photographerheaderSection = document.querySelector(".photographer-header");
   const photographerHeader__content = document.createElement("article");
   photographerHeader__content.className = "photographer__headerContent";
   photographerHeader__content.innerHTML = `
   <article class="photographer__info"> 
-    <h1 class="photographer__h1">${selectedPhotographer.name}</h1>
-    <p class="photographer__local">${selectedPhotographer.city}, ${selectedPhotographer.country}</p>
-    <p class="photographer__tagline">${selectedPhotographer.tagline}</p>
+    <h1 class="photographer__h1">${photographer.name}</h1>
+    <p class="photographer__local">${photographer.city}, ${photographer.country}</p>
+    <p class="photographer__tagline">${photographer.tagline}</p>
   </article>
   <button class="contact_button">Contactez-moi</button>
-  <img class="photographer__portrait" src="Sample_Photos/Photographers_ID_Photos/${selectedPhotographer.portrait}" alt="portrait de ${selectedPhotographer.name}"> `;
+  <img class="photographer__portrait" src="Sample_Photos/Photographers_ID_Photos/${photographer.portrait}" alt="portrait de ${photographer.name}"> `;
 
  photographerheaderSection.appendChild(photographerHeader__content);
-
-//  return getPhotographerData;
 }
-photographerHeader();
+
+const selectedPhotographer = await getPhotographerData (photographer_Id);
+console.log (selectedPhotographer);
+
+const getMediaData = async (photographerMedia_Id) => {
+  const data = await fetchPhotographers ();
+  const selectedMedia = data.media.filter ((media) => media.photographerId === photographerMedia_Id);
+  return selectedMedia;
+}
+
 async function photographerPortfolio () {
-const data = await fetchPhotographers ();
-const selectedMedia = data.media.filter ((media) => media.photographerId === photographer_Id) [0]
-console.log(selectedMedia);
+
+for ( let photographerMedia__content = 0; photographerMedia__content < selectedMedia.length; photographerMedia__content++){
   const photographerMediaSection = document.querySelector(".photographer__portfolio");
   const photographerMedia__content = document.createElement("article");
   photographerMedia__content.className = "photographer__portfolio--media";
+
   if (selectedMedia.video) {
   photographerMedia__content.innerHTML = `
    <video class="photographer__portfolio--media--content" poster="Sample_Photos/${selectedMedia.video}">
@@ -72,11 +72,25 @@ console.log(selectedMedia);
         <i class="fa-heart far photographer__portfolio--media--info--like--heart"></i>
       </div>
     </div>`;
-  }
-
+  } 
  photographerMediaSection.appendChild(photographerMedia__content);
-
+ }
 }
+
+const selectedMedia = await getMediaData (photographer_Id);
+console.log(selectedMedia);
+
+
+photographerHeader(selectedPhotographer);
+
+document.querySelector (".contact_button")
+.addEventListener ("click",function() {
+  displayModal ()
+} )
+
+document.querySelector (".closecross")
+.addEventListener ("click",function(){
+  closeModal ()
+})
+
 photographerPortfolio ();
-
-
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,19 +1,19 @@
-function displayModal() {
+export function displayModal() {
   const modal = document.getElementById("contact_modal");
 modal.style.display = "block";
 }
-function closeModal() {
+export function closeModal() {
   const modal = document.getElementById("contact_modal");
   modal.style.display = "none";
 } 
 
-document.querySelector (".contact_button")
-.addEventListener ("click",function() {
-  displayModal ()
-} )
+// document.querySelector (".contact_button")
+// .addEventListener ("click",function() {
+//   displayModal ()
+// } )
 
 
-document.querySelector (".closecross")
-.addEventListener ("click",function(){
-  closeModal ()
-})
+// document.querySelector (".closecross")
+// .addEventListener ("click",function(){
+//   closeModal ()
+// })
